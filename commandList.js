bb588bc7c4941c6b3d4997e8e1356bf04fb785f5
cbrower123 <31785077+cbrower123@users.noreply.github.com>
2017-09-09 01:07:36 -0400

    Add files via upload
    Voice command code

---
+++ commandList.js
@@ -0,0 +1,109 @@
+//Color Commands
+var red = {
+    'rojo': function() {
+        $('#name').html("That's right! Rojo!");
+    }
+};
+var blue = {
+    'azul': function() {
+        $('#name').html("That's right! Azul!");
+    }
+};
+var white = {
+    'blanco': function() {
+        $('#name').html("That's right! Blanco!");
+    },
+    'blanca': function() {
+        $('#name').html("That's right! Blanca!");
+    }
+};
+var yellow = {
+    'amarillo': function() {
+        $('#name').html("That's right! Amarillo!");
+    }
+};
+var green = {
+    'verde': function() {
+        $('#name').html("That's right! Verde!");
+    }
+};
+var orange = {
+    'naranja': function() {
+        $('#name').html("That's right! Naranja!");
+    }
+};
+var pink = {
+    'rosado': function() {
+        $('#name').html("That's right! Rosado!");
+    },
+    'rosa': function() {
+        $('#name').html("That's right! Rosa!");
+    },
+    'rojillo': function() {
+        $('#name').html("That's right! Rojillo!");
+    }
+};
+//Number Commands
+var zero = {
+    'cero': function() {
+        $('#name').html("That's right! Cero!");
+    }
+};
+var one = {
+    'uno': function() {
+        $('#name').html("That's right! Uno!");
+    }
+};
+var two = {
+    'dos': function() {
+        $('#name').html("That's right! Dos!");
+    }
+};
+var three = {
+    'tres': function() {
+        $('#name').html("That's right! Tres!");
+    }
+};
+var four = {
+    'cuatro': function() {
+        $('#name').html("That's right! Cuatro!");
+    }
+};
+var five = {
+    'cinco': function() {
+        $('#name').html("That's right! Cinco!");
+    }
+};
+var six = {
+    'seis': function() {
+        $('#name').html("That's right! Seis!");
+    }
+};
+var seven = {
+    'siete': function() {
+        $('#name').html("That's right! Siete!");
+    }
+};
+var eight = {
+    'ocho': function() {
+        $('#name').html("That's right! Ocho!");
+    }
+};
+var nine = {
+    'nueve': function() {
+        $('#name').html("That's right! Nueve!");
+    }
+};
+var ten = {
+    'diez': function() {
+        $('#name').html("That's right! Diez!");
+    }
+};
+var commands = {
+    'Mi nombre es': function() {
+        $('#name').html("Cece");
+    }
+};
+
+
+
